c8b7805b93d2741ac3194d897441362667d509d6
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2023-04-23 14:14:40 +0530

    remove unused files

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-import '../styles/globals.css';
-import '../styles/vendor/css/font-awesome.min.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
